3eda86a1bba319d16c84391df6da2795f03a345a
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-06-11 14:51:12 -0400

    commit tree 575b6ea0329421282cd4da244abc7b5e3dba6ce4

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 // Your code here
-const dodger = document.getElementById("dodger");
+//const dodger = document.getElementById("dodger");
+
+//dodger.style.backgroundColor = "#FF69B4";
+//dodger.style.bottom = "0px";
+//dodger.style.left = "0px";
 
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
+}
 
 document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+moveDodgerLeft();
 
   function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
@@ -26,7 +32,39 @@ document.addEventListener("keydown", function (e) {
   }
 
 document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
+    if (e.key === "ArrowRight") {
       moveDodgerRight();
     }
   });
+
+moveDodgerRight();
+
+//function moveDodgerLeft() {
+//    const leftNumbers = dodger.style.left.replace("px", "");
+//    const left = parseInt(leftNumbers, 10);
+//  
+//    if (left > 0) {
+//      dodger.style.left = `${left - 1}px`;
+//    }
+//  }
+
+//document.addEventListener("keydown", function (e) {
+//    if (e.key === "ArrowLeft") {
+//      moveDodgerLeft();
+//    }
+//  });
+
+//  function moveDodgerRight() {
+//    const leftNumbers = dodger.style.left.replace("px", "");
+//    const left = parseInt(leftNumbers, 10);
+//  
+//    if (left > 0) {
+//      dodger.style.left = `${left + 1}px`;
+//    }
+//  }
+
+//document.addEventListener("keydown", function (e) {
+//    if (e.key === "ArrowRight") {
+//      moveDodgerRight();
+//    }
+//  });
